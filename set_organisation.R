f5bd83fbf7c243c0a99b1a837204f49fbb1bc51c
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2025-06-20 10:55:51 +0200

    ✨ Define organisation

---
+++ set_organisation.R
@@ -0,0 +1,14 @@
+library(checklist)
+org_item$new(
+  name = c(`en-GB` = "muscardinus"),
+  email = "info@muscardinus.be",
+  rightsholder = "optional",
+  funder = "optional",
+  license = list(
+    package = c(`GPL-3.0` = "generic_template/gplv3.md"),
+    project = c(`CC-BY-SA-4.0` = "generic_template/cc_by_sa_4_0.md"),
+    data = c(`CC-BY-SA-4.0` = "generic_template/cc_by_sa_4_0.md")
+  )
+) |>
+  org_list$new() -> org
+org$write()
