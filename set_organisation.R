e943dd6ad4c495eef109b550a7f0ed759ccffc34
Thierry Onkelinx <thierry.onkelinx@inbo.be>
2025-11-01 15:30:12 +0100

    📄 Add licenses

--- set_organisation.R
+++ set_organisation.R
@@ -5,9 +5,27 @@ org_item$new(
   rightsholder = "optional",
   funder = "optional",
   license = list(
-    package = c(`GPL-3.0` = "generic_template/gplv3.md"),
-    project = c(`CC-BY-SA-4.0` = "generic_template/cc_by_sa_4_0.md"),
-    data = c(`CC-BY-SA-4.0` = "generic_template/cc_by_sa_4_0.md")
+    package = c(
+      `GPL-3.0` = paste(
+        "https://raw.githubusercontent.com/inbo/checklist/refs/heads/main/inst",
+        "generic_template/gplv3.md",
+        sep = "/"
+      )
+    ),
+    project = c(
+      `CC BY-SA 4.0` = paste(
+        "https://raw.githubusercontent.com/inbo/checklist/refs/heads/main/inst",
+        "generic_template/cc_by_sa_4_0.md",
+        sep = "/"
+      )
+    ),
+    data = c(
+      `CC BY-NC-SA 4.0` = paste(
+        "https://raw.githubusercontent.com/idleberg/Creative-Commons-Markdown",
+        "refs/heads/main/4.0/by-nc-sa.markdown",
+        sep = "/"
+      )
+    )
   )
 ) |>
   org_list$new(git = "https://github.com/ThierryO") -> org
